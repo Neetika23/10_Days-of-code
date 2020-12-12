98f84bc3e113f6e7c97c50afcef77843ff767d4e
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-12-12 12:27:49 +0530

    Create Problem_1283_Find_the_smallest_divisor.cpp

---
+++ Leetcode/Problem_1283_Find_the_smallest_divisor.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int getSum(vector<int> &nums, int divisor){
+        int tot = 0, i, n = nums.size();
+        for(int i=0;i<n;i++){
+            tot = tot+ceil((float)nums[i]/divisor);
+        }
+        return tot;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        //Find the maximum using running maximum.
+        
+        int n = nums.size(), max_val=INT_MIN, low, high, mid;
+        for(int i=0;i<n;i++){
+            max_val = max(max_val, nums[i]);
+        }
+        //Now search space is 1 to max value
+        //F*T*
+        //p(x): sum <= threshold
+        //first T
+        low = 1, high = max_val;
+        while(low<high){
+            mid = low + (high-low)/2;
+            if(getSum(nums,mid) <= threshold){
+                high = mid;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        
+        return low;
+    }
+};
